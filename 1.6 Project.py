046f5edcd48d6cde499048e16bd03ae6f14a7634
AsherMac17 <ashmac9358@students.ecpi.edu>
2026-09-03 09:41:58 -0400

    Add files via upload

---
+++ 1.6 Project.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+print("ashmac9358 Spreadsheet Automation Menu.")
+print("1. Input Data")
+print("2. View Current Data")
+print("3. Generate Report")
+
+# The next line retrieves the inputted option and stores into the variable called <variable>.
+option=input("Enter your option: ")
+
+print("Option", option, "has been selected.")
+print("the time and date is", str(datetime.now()))
+
